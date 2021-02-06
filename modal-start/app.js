24b696e93f00d6d7f0468a244608585b4883b269
Sridhar Kothapalli <sridr8@gmail.com>
2021-02-06 14:41:12 +0530

    modal functionality implemented

--- modal-start/app.js
+++ modal-start/app.js
@@ -1,1 +1,19 @@
-// modal
+const openModalTriggerEl = document.querySelector(".trigger");
+const closeModalTriggerEl = document.querySelector(".close");
+const modalEl = document.querySelector(".modal");
+
+function main() {
+  openModalTriggerEl.addEventListener("click", function () {
+    modalEl.classList.add("open");
+  });
+
+  closeModalTriggerEl.addEventListener("click", function () {
+    modalEl.classList.remove("open");
+  });
+
+  modalEl.addEventListener("click", function (event) {
+    if (event.target === modalEl) modalEl.classList.remove("open");
+  });
+}
+
+main();

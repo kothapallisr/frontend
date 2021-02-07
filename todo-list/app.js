f57c9e8923fb2471211e2980fd05f9c48b2076f7
Sridhar Kothapalli <sridr8@gmail.com>
2021-02-07 12:25:28 +0530

    todo list starter files

---
+++ todo-list/app.js
@@ -0,0 +1,4 @@
+const todoInputEl = document.querySelector('.todo__input');
+const todoListEl = document.querySelector('.todo__list');
+const todoItemEls = document.querySelectorAll('.todo__item');
+

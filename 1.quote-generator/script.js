540265744bc9f759a1ead2deea74d1c13447ae28
Sridhar Kothapalli <sridr8@gmail.com>
2021-07-13 21:06:51 +0530

    added loader

---
+++ 1.quote-generator/script.js
@@ -0,0 +1,66 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
+
+let apiQuotes = [];
+
+// Show loading
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+// Hide Loading
+function complete() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
+// Show new Quote
+function newQuote() {
+  loading();
+  // Pick a random quote form apiQuotes array
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  // Check if author field is blank and replace it with 'Unknokwn'
+  if (!quote.author) {
+    authorText.textContent = "Unknown";
+  } else {
+    authorText.textContent = quote.author;
+  }
+  // Check Quote length to determine styling
+  if (quote.text.length > 120) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+  // Set quote, Hide Loader
+  quoteText.textContent = quote.text;
+  complete();
+}
+
+// Get Quotes From API
+async function getQuotes() {
+  loading();
+  const apiUrl = "https://type.fit/api/quotes";
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    //console.log(apiQuotes[12]);
+    newQuote();
+  } catch (error) {
+    // catch error here
+  }
+}
+
+// Tweet Quote
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, "_blank");
+}
+// Event Listeners
+newQuoteBtn.addEventListener("click", newQuote);
+twitterBtn.addEventListener("click", tweetQuote);
+
+// On Load
+getQuotes();

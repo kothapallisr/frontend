a25b85b21b91a940c73071e0e0b8c435c43602de
Sridhar Kothapalli <sridr8@gmail.com>
2022-02-01 19:30:10 +0530

    initial commit

---
+++ js-projects/app.js
@@ -0,0 +1,8 @@
+// test truthy and falsy
+
+const text = ' ';
+if(text) {
+  console.log('Hey it\'s a truthy')
+} else {
+  console.log('Hey it\'s a falsy')
+}
--- jsbc/function-expressions.js
+++
@@ -1,8 +0,0 @@
-var i = 1;
-const timing = setInterval(function () {
-  console.log("Here is message number " + i);
-  i = i + 1;
-  if (i == 6) {
-    clearInterval(timing);
-  }
-}, 2000);

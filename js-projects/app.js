d4dd642b25ca9374de89b5958f79e9d51d860479
Sridhar Kothapalli <sridr8@gmail.com>
2022-02-02 16:31:42 +0530

    callback, higher order function

--- js-projects/app.js
+++ js-projects/app.js
@@ -1,8 +1,29 @@
 // test truthy and falsy
 
-const text = ' ';
-if(text) {
-  console.log('Hey it\'s a truthy')
-} else {
-  console.log('Hey it\'s a falsy')
+// const text = ' ';
+// if(text) {
+//   console.log('Hey it\'s a truthy')
+// } else {
+//   console.log('Hey it\'s a falsy')
+// }
+
+// call back, higher order functions
+
+function goodMorning (name) {
+  return `Good morning! ${name}`;
 }
+
+// callback function which is an argument for the higher order function. 
+function goodEvening (name) {
+  return `Good evening! ${name}`;
+}
+
+// higher order function (which takes the callback function as parameter)
+const greet = function(name, cb) {
+  const myName = 'John';
+  const message = cb(name);
+  console.log(`${message}. My name is ${myName}`);
+}
+
+greet("Susan", goodMorning);
+greet("Carey", goodEvening);

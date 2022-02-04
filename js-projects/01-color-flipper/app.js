8e96e57c34cbfa02f677f6ff3cd5040be01ebc97
Sridhar Kothapalli <sridr8@gmail.com>
2022-02-04 11:44:29 +0530

    simple version

--- js-projects/01-color-flipper/app.js
+++ js-projects/01-color-flipper/app.js
@@ -1,1 +1,14 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colors = ["green", "orange", "rgba(133,122,200)", "#f15025"];
+
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+btn.addEventListener('click', () => {
+  const randomNumer = getRandomNumber();
+  document.body.style.backgroundColor = colors[randomNumer];
+  color.textContent = colors[randomNumer];
+})
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * colors.length);
+}

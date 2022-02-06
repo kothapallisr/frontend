11e882d89a628dc37c01f3fea9edf0ab59a511e4
Sridhar Kothapalli <sridr8@gmail.com>
2022-02-06 20:39:04 +0530

    call apply bind code updated

--- javascript/call-apply-bind/app.js
+++ javascript/call-apply-bind/app.js
@@ -3,8 +3,8 @@ const john = {
   age: 33
 };
 
-function message() {
-  console.log(`My name is ${this.name} and I'm ${this.age} years old`);
+function message(city, area) {
+  console.log(`My name is ${this.name}, I'm ${this.age} years old and I live in ${city}, ${area}`);
 }
 
 const peter = {
@@ -12,3 +12,16 @@ const peter = {
   age: 35
 }
 
+// call() takes list of arguments and invokes the function
+message.call(john, 'Berlin', 'Germany');
+message.call(peter, 'Bay area', 'SF');
+
+// apppy() takes array of arguments and invokes the function
+message.apply(john, ['Berlin', 'Germany']);
+message.apply(peter, ['Bay area', 'SF']);
+
+// bind() takes list of arguments and returns a function
+const msg = message.bind(john, 'Berlin', 'Germany');
+msg();
+const msg2 = message.bind(peter, 'Bay area', 'SF');
+msg2();

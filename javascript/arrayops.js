5e15e15e72566de1e8e1ea9df6c91366d865335e
Sridhar Kothapalli <sridr8@gmail.com>
2022-05-30 14:52:47 +0530

    array operations

---
+++ javascript/arrayops.js
@@ -0,0 +1,15 @@
+let arr = ['one', 'two', 'three', 'four', 'five'];
+//console.log(arr.splice(2,0, 'new'));
+//console.log(arr);
+
+//console.log(arr.splice(5,0,'six'));
+//console.log(arr);
+
+//console.log(arr.splice(0,0,'zero'));
+//console.log(arr);
+
+//console.log(arr.splice(2,1,'new'));
+//console.log(arr);
+
+console.log(arr.splice(2,0,'another'));
+console.log(arr);

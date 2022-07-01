bf669bfa16bd7cd67941e2dd30e665543277d332
Sridhar Kothapalli <sridr8@gmail.com>
2022-07-01 14:28:55 +0530

    os module

--- node-app/app.js
+++ node-app/app.js
@@ -1,5 +1,14 @@
+const os = require('os');
+
+const totalMemory = os.totalmem();
+const freeMemory = os.freemem();
+
+
 function hello(name) {
   console.log(`Hello ${name}`);
 }
 
 hello('Sridhar')
+
+console.log(`Total memory: ${totalMemory}`);
+console.log(`Free memory: ${freeMemory}`)

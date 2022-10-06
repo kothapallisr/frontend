21097a3b9e7b64609541f4dfe7f820ccf9bb5c42
Sridhar Kothapalli <sridr8@gmail.com>
2022-10-06 17:24:33 +0530

    frontend

---
+++ express-demo/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!!!');
+})
+
+app.get('/api/courses', (req, res) => {
+  res.send([1,2,3]);
+})
+
+app.get('/api/posts/:year/:month', (req, res) => {
+  //res.send(req.params);
+  res.send(req.query);
+})
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Listening at port ${port}.....`);
+})

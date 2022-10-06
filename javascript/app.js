6d3780bdac3c46dbc253bd48531191d3a855d7ef
Sridhar Kothapalli <sridr8@gmail.com>
2022-10-06 17:27:44 +0530

    clean up

--- javascript/app.js
+++ javascript/app.js
@@ -9,21 +9,34 @@
 
 // call back, higher order functions
 
-function goodMorning (name) {
-  return `Good morning! ${name}`;
-}
+// function goodMorning (name) {
+//   return `Good morning! ${name}`;
+// }
 
-// callback function which is an argument for the higher order function. 
-function goodEvening (name) {
-  return `Good evening! ${name}`;
-}
+// // callback function which is an argument for the higher order function. 
+// function goodEvening (name) {
+//   return `Good evening! ${name}`;
+// }
 
-// higher order function (which takes the callback function as parameter)
-const greet = function(name, cb) {
-  const myName = 'John';
-  const message = cb(name);
-  console.log(`${message}. My name is ${myName}`);
-}
+// // higher order function (which takes the callback function as parameter)
+// const greet = function(name, cb) {
+//   const myName = 'John';
+//   const message = cb(name);
+//   console.log(`${message}. My name is ${myName}`);
+// }
 
-greet("Susan", goodMorning);
-greet("Carey", goodEvening);
+// greet("Susan", goodMorning);
+// greet("Carey", goodEvening);
+
+let people = [
+  {name: 'bob', position:'developer'},
+  {name: 'tom', position:'designer'},
+  {name: 'lam', position:'architect'},
+]
+
+//forEach
+people.forEach(item => {
+  console.log(item.position.toUpperCase())
+})
+
+

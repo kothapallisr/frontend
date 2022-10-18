c5d4e8d020ebe6e7acc07b328dd7f60d1a2c5c0e
Sridhar Kothapalli <sridr8@gmail.com>
2022-10-18 16:18:44 +0530

    recursion and currying

---
+++ javascript/js/recursion.js
@@ -0,0 +1,17 @@
+// sum(1)(2)(3)(4)
+let sum = function(a) {
+  return function(b) {
+    if(b) {
+      return sum(a+b)
+    }
+    return a;
+  }
+}
+console.log(sum(1)(2)(3)(4)())
+
+
+// using arrow function
+
+let sumArrow = a => b => b ? sumArrow(a + b) : a
+
+console.log(sumArrow(10)(1)(2)(3)(4)(5)())

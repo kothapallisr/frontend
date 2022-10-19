3a17e1b3271c117f7b7b8172309517d6fe088218
Sridhar Kothapalli <sridr8@gmail.com>
2022-10-19 11:05:30 +0530

    initial commit

---
+++ javascript/js/prototype.js
@@ -0,0 +1,15 @@
+let obj = {
+  name: 'Sridhar',
+  city: 'Munich',
+  getDetails: function() {
+    console.log(`${this.name} from ${this.city}`)
+  }
+}
+
+let obj2 = {
+  name: 'Saisuhas',
+  city: 'Berlin'
+}
+
+
+

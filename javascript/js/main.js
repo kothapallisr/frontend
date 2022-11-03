63f2ebc4028a40f823e66e9fdbe4018b4b0a205c
Sridhar Kothapalli <sridr8@gmail.com>
2022-11-03 16:00:36 +0530

    test

--- javascript/js/main.js
+++ javascript/js/main.js
@@ -1,10 +1,20 @@
-let a = 10
-function outer() {
-  let b = 20
-  function inner() {
-    let c = 30
-    console.log(a,b,c)
-  }
-  inner()
+function foo(num) {
+	console.log( "foo: " + num );
+
+	// keep track of how many times `foo` is called
+	data.count++;
+  
 }
-outer()
+var data = {
+  count: 0
+}
+foo.count = 0;
+
+var i;
+
+for (i=0; i<10; i++) {
+	if (i > 5) {
+		foo( i );
+	}
+}
+console.log(data.count)
